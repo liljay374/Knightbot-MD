cdec49ec13a90b36ee9ac3d11819220faf4ceba5
liljay374 <gwenzilizzy46@gmail.com>
2025-05-31 16:11:01 +0200

    Update settings (1).js

--- settings (1).js
+++ settings (1).js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Lil Jay', // Your name
+  ownerNumber: '263772463913X', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
