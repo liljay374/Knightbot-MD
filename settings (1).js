aaba9fb23ce9ef0d905c51f574a46d5b405f9ce7
liljay374 <gwenzilizzy46@gmail.com>
2025-05-31 16:09:15 +0200

    Add files via upload

---
+++ settings (1).js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight',
+  author: 'Bot',
+  botName: "Knight Bot",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
